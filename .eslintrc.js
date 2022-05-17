7f0a06659c5fcc5bf5058788e9bc2fbdc361e6b6
ellmc2 <jellmc2@qq.com>
2022-05-17 15:31:17 +0800

    chore: ts-study-demo

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    extends: [
+        'alloy',
+        'alloy/typescript',
+    ],
+    env: {
+        // 您的环境变量（包含多个预定义的全局变量）
+        // Your environments (which contains several predefined global variables)
+        //
+        // browser: true,
+        // node: true,
+        // mocha: true,
+        // jest: true,
+        // jquery: true
+    },
+    globals: {
+        // 您的全局变量（设置为 false 表示它不允许被重新赋值）
+        // Your global variables (setting to false means it's not allowed to be reassigned)
+        //
+        // myGlobal: false
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {
+        // 禁止使用 var
+        'no-var': "error",
+        // 优先使用 interface 而不是 type
+        '@typescript-eslint/consistent-type-definitions': [
+            "error",
+            "interface"
+        ]
+    }
+}
